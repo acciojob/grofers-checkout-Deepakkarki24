fed6e64a4f196942742902c3b413f74c96a9af69
Deepak karki <ideepakkarki@gmail.com>
2025-01-06 11:38:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,24 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  //Add your code here
+  let prices = document.querySelectorAll(".price");
+  let table = document.querySelector("table");
+
+  let sum = 0;
+  prices.forEach((price) => {
+    sum += parseInt(price.textContent);
+  });
+
+  let lastRow = document.createElement("tr");
+  let rowLabel = document.createElement("td");
+  let rowValue = document.createElement("td");
+
+  rowLabel.innerHTML = "total";
+  rowValue.innerHTML = sum;
+
+  lastRow.append(rowLabel, rowValue);
+  table.append(lastRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
